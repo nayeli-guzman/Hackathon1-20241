ab12e670683f879a8d00e91a6604369a83dd1681
Salva <atlas200x@gmail.com>
2024-09-28 16:51:50 -0500

    undoxing nayeli

--- hackathon/01/Hackathon1-20241-main/Hackathon1-20241-main/src/main/java/dbp/hackathon/funcion/infrastructure/FuncionRepository.java
+++
@@ -1,7 +0,0 @@
-package dbp.hackathon.funcion.infrastructure;
-
-import dbp.hackathon.funcion.domain.Funcion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FuncionRepository extends JpaRepository<Funcion, Long> {
-}
